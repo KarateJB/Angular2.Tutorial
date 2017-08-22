57ed2a49655406feecb0a17515a388f68e4199b1
KarateJB <ppippenkimo@gmail.con>
2017-08-22 11:07:30 +0800

    Add comment on Mater after rebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
 
 
   ngOnInit() {
-    this.title = "Customers"; //Add comment on Branch 2
+    this.title = "Customers"; //Add comment on Mater after rebase
     // this.defaultData = this.data = customers;
   };
 
